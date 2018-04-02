c0b5bd8e1faa1a0591590f45c5bf1ded7a866fb6
m-mma <mark.muema@gmail.com>
2018-04-03 00:35:05 +0300

    updated custom message on header

--- wp-content/themes/weddingvendor/header.php
+++ wp-content/themes/weddingvendor/header.php
@@ -20,7 +20,7 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php 
+<?php
 $header_search_btn=tg_get_option('header_search_btn');
 if(isset($header_search_btn) && !empty($header_search_btn))
 {
@@ -32,9 +32,9 @@ if(isset($header_search_btn) && !empty($header_search_btn))
     <span class="input-group-btn">
     <button class="btn tp-btn-primary" type="submit"><?php esc_html_e( 'Search', 'weddingvendor' ); ?></button>
     </span> </div>
-</form>    
+</form>
 </div>
-<?php 
+<?php
 }
 ?>
 <!-- /.top search -->
@@ -42,54 +42,54 @@ if(isset($header_search_btn) && !empty($header_search_btn))
   <div class="container">
     <div class="row">
       <div class="col-md-4 top-message">
-        <p><?php esc_html_e( 'Welcome to Wedding Vendor', 'weddingvendor' ); ?></p>
+        <p><?php esc_html_e( 'The Online Marketplace For Weddings in East Africa', 'weddingvendor' ); ?></p>
       </div>
       <div class="col-md-8 top-links">
-		<?php wp_nav_menu( array( 'theme_location' => 'topbar', 'menu_class' => 'listnone',  'fallback_cb' => false ) ); ?>              
+		<?php wp_nav_menu( array( 'theme_location' => 'topbar', 'menu_class' => 'listnone',  'fallback_cb' => false ) ); ?>
       </div>
     </div>
   </div>
-</div>    
+</div>
 
 <div class="tp-nav" id="headersticky"><!-- navigation start -->
   <div class="container">
-    <nav class="navbar navbar-default navbar-static-top">       
-      <!-- Brand and toggle get grouped for better mobile display -->      
+    <nav class="navbar navbar-default navbar-static-top">
+      <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-		<?php 
+		<?php
         if(tg_get_option('checker')!=""){ ?>
         <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php } else{ ?>		
+            <?php } else{ ?>
         <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
         <?php if(tg_get_option('logo')!=""){ 	?>
             <img src="<?php echo esc_url(tg_get_option('logo')); ?>" alt="<?php bloginfo( 'name' ); ?>" class="img-responsive">
-        <?php 	
+        <?php
         } else {?>
             <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" class="img-responsive">
         <?php } ?>
-        </a>      
-        <?php } ?> 
-        </div>      
+        </a>
+        <?php } ?>
+        </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="navigation">
             <div class="" id="cssmenu">
                 <?php
                     if ( has_nav_menu( 'primary' ) ) {
-                        wp_nav_menu( array( 
+                        wp_nav_menu( array(
                             'theme_location' => 'primary',
                               'container'=>false,
                               'walker'=>new weddingvendor_Tg_Menu(),
                               'menu_class'=>'nav navbar-nav navbar-right',
-                            ) 
+                            )
                         );
                     }
-                ?>              
-            </div>      <!-- /.navbar-collapse -->       
+                ?>
+            </div>      <!-- /.navbar-collapse -->
         </div>
     </nav>
   </div>
-  <!-- /.container-fluid -->   
+  <!-- /.container-fluid -->
 </div>
-<?php 
+<?php
 get_template_part( 'template-parts/user/header', 'banner' );
-?>  
+?>
