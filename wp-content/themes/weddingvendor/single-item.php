58f68588295baafa4f81ef49a68467e1a7c4f8ed
m-mma <mark.muema@gmail.com>
2018-06-17 17:55:58 +0300

    added a single item child page template

--- wp-content/themes/weddingvendor/single-item.php
+++ wp-content/themes/weddingvendor/single-item.php
@@ -7,9 +7,11 @@
  * @package weddingvendor
  */
 
-get_header(); 
+get_header();
 
 $list_style=tg_get_option('items_display_style');
-get_template_part( 'template-parts/item/vendor', $list_style );
+get_template_part( '../weddingvendor-child/template-parts/item/vendor', $list_style );
+// get_template_part( 'template-parts/item/vendor', $list_style );
+// D:\MAMP\htdocs\boab-site\wp-content\themes\weddingvendor-child\template-parts\item\vendor-right.php
 
 get_footer(); ?>
